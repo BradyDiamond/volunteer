b4f07fd91ffc90f385e0b22112d003dd44af0daf
brady diamond <brady.diamond27@gmail.com>
2022-05-20 08:29:10 -0700

    add test files

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require "volunteer"
+require "project"
+require "rspec"
+require "pry"
+require "pg"
+
+DB = PG.connect({:dbname => 'volunteer_tracker_test', host: 'db', user: 'postgres', password: 'password'  })
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec('DELETE FROM volunteers *;')
+    DB.exec('DELETE FROM projects *;')
+  end
+end
